d27674e98e9f9e525554a5c5ec1a90eff9858ed7
andresListorti <andreslistorti@gmail.com>
2023-03-20 13:50:18 -0300

    Final guia 2 Extras Todos

--- src/guia2EjerciciosExtra10.java
+++ src/guia2EjerciciosExtra10.java
@@ -8,6 +8,40 @@ import java.util.Scanner;
 ///
 public class guia2EjerciciosExtra10 {
     public static void main(String[] args) {
+        double aleatorio1, aleatorio2, multiplo, respuesta;
+        int confirma;
+        Scanner leer = new Scanner(System.in);
+        aleatorio1 = Math.floor(Math.random() * 10);
+        aleatorio2 = Math.floor(Math.random() * 10);
+        multiplo = aleatorio1 * aleatorio2;
+
+        // System.out.println(aleatorio1);
+        // System.out.println(aleatorio2);
+        // System.out.println(multiplo);
+
+        confirma = 1;
+
+        do {
+
+            System.out.println("Ingrese el resultado de la multiplicacion de los 2 numeros generados aleatoriamente: ");
+            respuesta = leer.nextDouble();
+
+            if (respuesta == multiplo) {
+                System.out.println("Respuesta Correcta!!!");
+                break;
+
+            } else {
+                System.out.println("Respuesta Incorrecta.");
+                System.out.println("Desea intentar nuevamente? - Ingrese 1 para Si, Ingrese 2 Para No: ");
+                confirma = leer.nextInt();
+                if (confirma == 1) {
+                    System.out.println("Buena suerte la proxima! ");
+                } else {
+                    break;
+                }
+
+            }
+        } while (confirma == 1);
 
     }
 }
